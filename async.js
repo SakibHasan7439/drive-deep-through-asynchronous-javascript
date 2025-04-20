e4c0af05b42c282eda3da9517c34298fb8aed6a2
sakibHasan7439 <sakibhasanb120@gmail.com>
2025-04-21 00:23:25 +0600

    practice asynchronous promise

--- async.js
+++ async.js
@@ -23,4 +23,21 @@ function doOperation() {
     })
 }
 
-doOperation();
+// doOperation();
+
+const fetchPromise = fetch(
+    "bad-scheme://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+  );
+  
+  fetchPromise.then((response) => {
+    if(!response.ok){
+        throw new Error(`HTTP error: ${response.status}`);
+    }
+    response.json();
+  })
+  .then((data) =>{
+    console.log(data[0].price);
+  })
+  .catch((error) =>{
+    console.log(`Could not got products: ${error}`);
+  })
